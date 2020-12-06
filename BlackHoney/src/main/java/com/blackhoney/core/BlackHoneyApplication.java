366a9b1aa3ca7816c15afdb2432e78a7e8214c3f
israel <israel@192.168.0.112>
2020-12-06 17:58:14 -0600

    Initial Commit

---
+++ BlackHoney/src/main/java/com/blackhoney/core/BlackHoneyApplication.java
@@ -0,0 +1,13 @@
+package com.blackhoney.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackHoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackHoneyApplication.class, args);
+	}
+
+}
